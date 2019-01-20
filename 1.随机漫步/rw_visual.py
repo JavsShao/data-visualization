89ed565da523071f6b23977f0c3d69e880022227
hamdi <javs_shao@163.com>
2019-01-20 16:28:23 +0800

    模拟多次随机漫步

--- 1.随机漫步/rw_visual.py
+++ 1.随机漫步/rw_visual.py
@@ -2,11 +2,15 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
+while True:
+    # 创建一个RandomWalk实例, 并将其中包含的点都绘制出来
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
 
-# 创建一个RandomWalk实例, 并将其中包含的点都绘制出来
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    keep_running = input('Make another walk?(y/n):')
+    if keep_running == 'n':
+        break
 
-plt.savefig('绘制随机漫步.png', bbox_inches='tight')
+# plt.savefig('绘制随机漫步.png', bbox_inches='tight')
