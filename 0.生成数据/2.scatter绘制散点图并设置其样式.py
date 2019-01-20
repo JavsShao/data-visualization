62433addbf93c93f805613c75f0946f003e7055a
hamdi <javs_shao@163.com>
2019-01-20 15:14:21 +0800

    绘制简单的折线图_scatter绘制散点图并设置其样式_2

--- 0.生成数据/2.scatter绘制散点图并设置其样式.py
+++ 0.生成数据/2.scatter绘制散点图并设置其样式.py
@@ -1,5 +1,15 @@
 import matplotlib.pyplot as plt
 
 
-plt.scatter(2, 4)
+plt.scatter(2, 4, s=200)
+
+# 设置图标标题并给坐标加上标签
+plt.title('Square Numbers', fontsize=24)
+plt.xlabel('xValue', fontsize=14)
+plt.ylabel('yValue', fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis='both', which='major', labelsize=14)
+
+
 plt.show()
