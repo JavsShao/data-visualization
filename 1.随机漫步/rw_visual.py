22c29b07999be91b6956a46b050d8ae00cfa7d1d
hamdi <javs_shao@163.com>
2019-01-20 16:43:19 +0800

    设置随机漫步图的样式_给点着色_突出起点和终点

--- 1.随机漫步/rw_visual.py
+++ 1.随机漫步/rw_visual.py
@@ -9,10 +9,14 @@ while True:
 
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=125)
+
+    # 突出起点和终点
+    plt.scatter(0, 0, c='yellow', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
     plt.show()
 
     keep_running = input('Make another walk?(y/n):')
     if keep_running == 'n':
         break
 
-plt.savefig('给点着色.png', bbox_inches='tight')
+# plt.savefig('给点着色.png', bbox_inches='tight')
