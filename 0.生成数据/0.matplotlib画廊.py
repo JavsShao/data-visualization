ddfa5d6aaa0b9e6bcfb65622c81751fb8fb0d096
hamdi <javs_shao@163.com>
2019-01-20 14:46:39 +0800

    绘制简单的折线图_修改标签文字和线条粗细

--- 0.生成数据/0.matplotlib画廊.py
+++ 0.生成数据/0.matplotlib画廊.py
@@ -2,5 +2,14 @@ import matplotlib.pyplot as plt
 
 
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+plt.plot(squares, linewidth=5)
+
+# 设置图标标题, 并给坐标轴加上标签
+plt.title("Squares values")
+plt.xlabel("x", fontsize=14)
+plt.ylabel("y", fontsize=14)
+
+# 设置刻度标记的大小
+plt.tick_params(axis='both', labelsize=14)
+
 plt.show()
