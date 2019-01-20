4e08fcfac8b1e0976b90f31068e25869e4a7503a
hamdi <javs_shao@163.com>
2019-01-20 16:37:40 +0800

    设置随机漫步图的样式_给点着色

--- 1.随机漫步/rw_visual.py
+++ 1.随机漫步/rw_visual.py
@@ -6,11 +6,13 @@ while True:
     # 创建一个RandomWalk实例, 并将其中包含的点都绘制出来
     rw = RandomWalk()
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=125)
     plt.show()
 
     keep_running = input('Make another walk?(y/n):')
     if keep_running == 'n':
         break
 
-# plt.savefig('绘制随机漫步.png', bbox_inches='tight')
+plt.savefig('给点着色.png', bbox_inches='tight')
