d69754cf85df83686f4b9db0894d72ca244cf310
hamdi <javs_shao@163.com>
2019-01-23 11:52:51 +0800

    CSV数据_在图表中添加日期

--- 3.下载数据/0.highs_lows.py
+++ 3.下载数据/0.highs_lows.py
@@ -1,6 +1,7 @@
 import csv
 
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 
 # 从文件中获取最高气温
@@ -9,8 +10,10 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
         high = int(row[1])
         highs.append(high)
 
@@ -18,11 +21,12 @@ with open(filename) as f:
 
 # 根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 # 设置图形的格式
 plt.title("Daily high temperatures, July 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel('Temperatures(F)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
